3c70007c125ced696018973fd33320a16d145e46
bruno-f-cruz <7049351+bruno-f-cruz@users.noreply.github.com>
2026-02-13 20:29:32 -0800

    bump

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import os
-
-os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
-
+import matplotlib.pyplot as plt
+from pydantic_settings import CliApp
 from vr_foraging_sbi_ddm import pipeline
 from vr_foraging_sbi_ddm.models import Config
-CONFIG = Config(n_simulations=500000, batch_size=256, n_iter=100000, n_early_stopping_patience=20, force_retrain=True)
-pipeline.run_pipeline(config=CONFIG)
+os.environ["XLA_PYTHON_CLIENT_PREALLOCATE"] = "false"
+plt.rcParams['text.usetex'] = False  # disable LaTeX
+
+def main(settings: Config):
+    pipeline.run_pipeline(config=settings)
+
+
+if __name__ == "__main__":
+    #CONFIG = Config(n_simulations=500000, batch_size=256, n_iter=100000, n_early_stopping_patience=20, force_retrain=True, base_output_dir="./scratch")
+    config = CliApp().run(Config)
+    main(config)
